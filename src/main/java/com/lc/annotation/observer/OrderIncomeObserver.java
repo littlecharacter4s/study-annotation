a538b97a90e3653ff81dd5535a578eb6899eb195
gujixian <gujixian@daojia.com>
2018-03-23 19:56:41 +0800

    Add Observer Annotation!

--- src/main/java/com/lc/annotation/observer/OrderIncomeObserver.java
+++ src/main/java/com/lc/annotation/observer/OrderIncomeObserver.java
@@ -1,5 +1,6 @@
 package com.lc.annotation.observer;
 
+import com.lc.annotation.pojo.Result;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.springframework.stereotype.Component;
 
@@ -9,7 +10,10 @@ public class OrderIncomeObserver implements AccountObserver {
     public Object adjust(ProceedingJoinPoint joinPoint) {
         try {
             Object o = joinPoint.proceed();
-            System.out.println("OrderIncomeObserver:订单收入调整!");
+            Result result = (Result) o;
+            if (result.isSuccess()) {
+                System.out.println("OrderIncomeObserver:订单收入调整!" + result.getData());
+            }
             return o;
         } catch (Throwable throwable) {
             throwable.printStackTrace();
